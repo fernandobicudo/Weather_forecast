93c3c05fa40b45a2dbb2ffe265c9a8dae4f6036d
fernandobicudo <fernandobicudo@gmail.com>
2019-03-15 22:17:53 -0300

    Criado o adapter, que consulta o JSON, infla cada view e a preenche ordenadamente com o conteúdo do JSON.

--- app/src/main/java/com/fernando/weather_forecast/MainActivity.java
+++ app/src/main/java/com/fernando/weather_forecast/MainActivity.java
@@ -3,28 +3,30 @@ package com.fernando.weather_forecast;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText locationEditText;
+    private List<Weather> previsoes = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener((v)->{//expressão lambda substituindo o código original
             String cidade = locationEditText.getEditableText().toString();
             String endereco = getString(R.string.web_service_url, cidade, getString(R.string.api_key),
-                    getString(R.string.meaasurement_unit));
+                    getString(R.string.measurement_unit));
 
             new GetWeatherTask().execute(endereco);
         });
@@ -61,9 +63,30 @@ public class MainActivity extends AppCompatActivity {
                 while ((linha = reader.readLine()) !=null) {
                     resultado.append(linha);
                 }
+                JSONObject jsonInteiro = new JSONObject((resultado.toString()));
+                JSONArray list = jsonInteiro.getJSONArray("list");
+
+                for (int i = 0; i < list.length(); i++) {
+                    JSONObject iesimo = list.getJSONObject(i);
+                    long dt = iesimo.getLong("dt");
+                    JSONObject main = iesimo.getJSONObject("main");
+                    double temp_min = main.getDouble("temp_min");
+                    double temp_max = main.getDouble("temp_max");
+                    int humidity = main.getInt("humidity");
+                    JSONArray weather = iesimo.getJSONArray("weather");
+                    JSONObject unicoDoWeather = weather.getJSONObject(0);
+                    String description = unicoDoWeather.getString("description");
+                    String icon = unicoDoWeather.getString("icon");
+
+                    Weather w = new Weather(dt, temp_min, temp_max, humidity, description, icon);
+
+                    previsoes.add(w);
+
+                }
+
                 return resultado.toString();
 
-            } catch (IOException e) {
+            } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
             return null;
---
+++ app/src/main/java/com/fernando/weather_forecast/WeatherArrayAdapter.java
@@ -0,0 +1,46 @@
+package com.fernando.weather_forecast;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class WeatherArrayAdapter extends ArrayAdapter<Weather> {
+    WeatherArrayAdapter(Context context, List<Weather> previsoes) {
+        super(context, -1, previsoes);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View covnertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View caixa = inflater.inflate(R.layout.list_item, parent, false);
+        Weather caraDaVez = getItem(position);
+
+        TextView dayTextView = caixa.findViewById(R.id.dayTextView);
+
+        TextView lowTextView = caixa.findViewById(R.id.lowTextView);
+
+        TextView highTextView = caixa.findViewById(R.id.highTextView);
+
+        TextView humidityTextView = caixa.findViewById(R.id.umidityTextView);
+
+        dayTextView.setText(getContext().getString(R.string.day_description, caraDaVez.dayOfWeek, caraDaVez.description));
+
+        lowTextView.setText(getContext().getString(R.string.low_temp, caraDaVez.minTemp));
+
+        highTextView.setText(getContext().getString(R.string.high_temp, caraDaVez.maxTemp));
+
+        return caixa;
+    }
+
+}
